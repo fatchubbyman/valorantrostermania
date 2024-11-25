2630ee51ee256297de6049d6d1e49c6d4ebf4be2
chubby <jatintheboy@gmail.com>
2024-11-26 00:50:06 +0530

    Add files via upload

--- valorant.py
+++ valorant.py
@@ -26,6 +26,18 @@ def wait():
         print('.')
         time.sleep(0.4)
 
+def price_maker(href):
+    url = 'https://www.vlr.gg/' + href
+    rqsts = requests.get(url)
+    soup = BeautifulSoup(rqsts.content,'lxml')
+    a_tag = soup.find('a', style ='margin-top: 3px; display: block;')
+    twitter = a_tag.get('href')
+    rqsts2 = requests.get(twitter)
+    soup2 = BeautifulSoup(rqsts2.content,'lxml')
+    follow_tag = soup2.find('div', class_ = 'css-175oi2r r-13awgt0 r-18u37iz r-1w6e6rj')
+    followers = follow_tag.find('span', class_ = 'css-1jxf684 r-bcqeeo r-1ttztb7 r-qvutc0 r-poiln3')
+    print(followers.text.strip())
+
 def role_maker(agents):
     duelists = ['neon','raze','jett','phoenix','yoru','iso','reyna','chamber']
     controllers = ['brimstone','clove','omen','astra','harbor']
@@ -66,8 +78,8 @@ def region_maker(links,players,teams):
         for team in teams_tags:
             a_tag = team.find('a', class_ = 'wf-module-item event-team-name')
             teams[a_tag.text.strip()] = Team(name = a_tag.text.strip(),squad={})
-        stats_link = link.replace('/event','/event/stats')
-        stats_link = stats_link.replace('/regular-season','')
+            stats_link = link.replace('/event','/event/stats')
+            stats_link = stats_link.replace('/regular-season','')
         rqsts = requests.get(stats_link)
         soup = BeautifulSoup(rqsts.content,'lxml')
         trs = soup.find_all('tr')
@@ -91,16 +103,9 @@ def region_maker(links,players,teams):
             if href_tag:
                 href = href_tag.get('href')
             players[ign] = Player(kd=kd,acs=acs,href=href,prev=prev,role=role,ign=ign)
-        for key,value in teams.items():
-            for i in range(5):
-                random_player_key = rd.choice(list(players.keys()))
-                random_player_object = players.pop(random_player_key)
-                value.squad[random_player_key] = random_player_object
-
 
 
 links = ['https://www.vlr.gg/event/2094/champions-tour-2024-emea-stage-2/regular-season','https://www.vlr.gg/event/2005/champions-tour-2024-pacific-stage-2/regular-season','https://www.vlr.gg/event/2095/champions-tour-2024-americas-stage-2/regular-season']
-stats = {'emea':'https://www.vlr.gg/event/stats/2094/champions-tour-2024-emea-stage-2','apac':'https://www.vlr.gg/event/stats/2005/champions-tour-2024-pacific-stage-2','americas':'https://www.vlr.gg/event/stats/2095/champions-tour-2024-americas-stage-2'}
 wait()
 region_select = input('What region would you like to represent? (emea,apac,americas)')
 if region_select == 'emea':
@@ -111,9 +116,8 @@ else:
     region = links.pop(2)
 # your own region process
 class MyPlayer(Player):
-    def __init__(self, acs, kd, ign, prev, role, href,twitter_flwrs,price):
+    def __init__(self, acs, kd, ign, prev, role, href,price):
         super().__init__(acs, kd, ign, prev, role, href)
-        self.twitter_flwrs = twitter_flwrs
         self.price = price
 
 class MyTeam(Team):
@@ -121,9 +125,69 @@ class MyTeam(Team):
         super().__init__(name, squad)
         self.matches = matches
 
-region_maker(links=links,players=players,teams=teams)
 rqsts = requests.get(region)
 soup = BeautifulSoup(rqsts.content,'lxml')
 # display teams and then select a team and make the others into team objects
+team_body = soup.find('div', class_ = 'event-teams-container')
+teams_for_display = team_body.find_all('a',class_ = 'wf-module-item event-team-name')
+for i in range(len(teams_for_display)):
+    print(f'{i+1} {teams_for_display[i].text.strip()}')
+    print('.')
+team_choice = input('Which team would you like to play as? (enter the team number)')
+your_team = MyTeam(name = teams_for_display[team_choice].text.strip())
+for team in teams_for_display:
+    if team.text.strip() is not your_team.name:
+        teams[team.text.strip()] = Team(name= team.text.strip())
+stats_link = region.replace('/event','/event/stats')
+stats_link = stats_link.replace('/regular-season','')
+rqsts = requests.get(stats_link)
+soup = BeautifulSoup(rqsts,'lxml')
+trs = soup.find_all('tr')
+for tr in trs:
+    agents = []
+    ign = tr.find('div', style = 'font-weight: 700; margin-bottom: 2px; width: 90px;').text.strip()
+    data = tr.find_all('td')
+    acs = float(data[4].text.strip())
+    kd = float(data[5].text.strip())
+    # finding roles 
+    agents = tr.find('td', class_ = 'mod-agents')
+    img_tags = agents.find_all('img')
+    for img_tag in img_tags:
+        agent_src = img_tag.get('src')
+        agent_name = agent_src.replace('/img/vlr/game/agents/','')
+        agents.append(agent_name.replace('.png',''))
+    role = role_maker(agents)
+    prev = tr.find('div', class_ = 'stats-player-country').text.strip()
+    #href
+    href_tag = tr.find('a')
+    if href_tag:
+         href = href_tag.get('href')
+    price = price_maker(href)
+    players[ign] = MyPlayer(kd=kd,acs=acs,href=href,prev=prev,role=role,ign=ign,price=price) 
+
+#picking up a duelist, and displaying
+for key,value in players.items():
+    if value.role == 'duelist':
+        print(value.ign)
+        print(value.price)
+        print(value.acs)
+        print(value.kd)
+        print(value.prev)
+        print(value.role)
+duelist_pick = input('Select the player no. ')
+
+
+
+
+
+
+
+
+
+for key,value in teams.items():
+    for i in range(5):
+        random_player_key = rd.choice(list(players.keys()))
+        random_player_object = players.pop(random_player_key)
+        value.squad[random_player_key] = random_player_object
 
 
